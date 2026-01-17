63fefd5e72a92ba5f6161dd157eec3461681f5d7
reggieboi11 <reginald.wlsnnn87@icloud.com>
2026-01-17 07:05:23 -0500

    Initial Commit

---
+++ inventory app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  inventory app
+//
+//  Created by Reginald Wilson on 1/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ inventory app/inventory_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  inventory_appApp.swift
+//  inventory app
+//
+//  Created by Reginald Wilson on 1/17/26.
+//
+
+import SwiftUI
+
+@main
+struct inventory_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
